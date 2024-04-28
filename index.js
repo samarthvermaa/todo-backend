e5286208703c976ea5f7154e417daca93c06318c
Samarth Verma <samarth.verma@stationcasinos.com>
2024-04-28 20:02:05 +0530

    feat: cors added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { todoRouter } from "./routers/index.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/my", todoRouter);
 
